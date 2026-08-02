2149825333a627dabbc296885038f50c04067e83
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-08-02 22:18:24 +0600

    [Sync] Equal_Candies on Codeforces 🚀

---
+++ Codeforces/Codeforces_Round_790_(Div._4)/B_Equal_Candies.cpp
@@ -0,0 +1,41 @@
+/*
+ * Codeforces Auto Push - Synced Solution
+ * 
+ * Problem: 1676B - Equal_Candies
+ * Link: https://codeforces.com/contest/1676/problem/B
+ * Submission ID: 353289440
+ * Language: C++17 (GCC 7-32)
+ * Execution Time: 78 ms | Memory: N/A
+ * Verdict: OK
+ * Rating: 800 | Tags: greedy, math, sortings
+ * Submitted At: Sat, 13 Dec 2025 08:04:09 GMT
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int a[n];
+        int mn = INT_MAX;
+        for (int i=0;i<n;i++) 
+        {
+            cin >> a[i];
+            mn = min(mn,a[i]);
+        }
+        long long sum=0;
+        for (int i=0;i<n;i++) 
+        {
+            int x = a[i]-mn;
+            sum+=x;
+        }
+        cout << sum << endl;
+
+    }
+    return 0;
+}
