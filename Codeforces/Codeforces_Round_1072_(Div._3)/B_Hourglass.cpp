d2acfef66161ead10769c4a55543bc0d37be134e
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-08-02 22:20:35 +0600

    [Sync] Hourglass on Codeforces 🚀

---
+++ Codeforces/Codeforces_Round_1072_(Div._3)/B_Hourglass.cpp
@@ -0,0 +1,50 @@
+/*
+ * Codeforces Auto Push - Synced Solution
+ * 
+ * Problem: 2184B - Hourglass
+ * Link: https://codeforces.com/contest/2184/problem/B
+ * Submission ID: 357485816
+ * Language: C++17 (GCC 7-32)
+ * Execution Time: 15 ms | Memory: N/A
+ * Verdict: OK
+ * Rating: 1100 | Tags: math
+ * Submitted At: Mon, 12 Jan 2026 14:54:41 GMT
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    long long s, k, m;
+    if (!(cin >> s >> k >> m)) return;
+
+    long long flips = m/k;
+    long long elapsed_in_interval = m%k;
+
+    long long sand_start;
+    if (flips % 2 == 0) {
+        sand_start = s;
+    } else {
+        sand_start = min(s, k);
+    }
+
+    if (elapsed_in_interval == 0) {
+        cout << sand_start << endl;
+    } else {
+        long long ans = max(0LL, sand_start - elapsed_in_interval);
+        cout << ans << endl;
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    if (cin >> t) {
+        while (t--) {
+            solve();
+        }
+    }
+    return 0;
+}
