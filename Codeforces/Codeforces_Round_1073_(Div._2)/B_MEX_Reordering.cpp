25fe7b5c44dc1696a4ae6b5ef13d8b83f6adc769
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-08-02 22:20:48 +0600

    [Sync] MEX_Reordering on Codeforces 🚀

---
+++ Codeforces/Codeforces_Round_1073_(Div._2)/B_MEX_Reordering.cpp
@@ -0,0 +1,41 @@
+/*
+ * Codeforces Auto Push - Synced Solution
+ * 
+ * Problem: 2191B - MEX_Reordering
+ * Link: https://codeforces.com/contest/2191/problem/B
+ * Submission ID: 358297464
+ * Language: C++17 (GCC 7-32)
+ * Execution Time: 31 ms | Memory: N/A
+ * Verdict: OK
+ * Rating: 1000 | Tags: constructive algorithms, sortings
+ * Submitted At: Sat, 17 Jan 2026 15:10:21 GMT
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int c0 = 0;
+        int c1 = 0;
+        for (int i = 0; i < n; ++i) {
+            int x;
+            cin >> x;
+            if (x == 0) c0++;
+            if (x == 1) c1++;
+        }
+
+        if (c0 == 0) {
+            cout << "NO" << "\n";
+        } else if (c0 > 1 && c1 == 0) {
+            cout << "NO" << "\n";
+        } else {
+            cout << "YES" << "\n";
+        }
+    }
+    return 0;
+}
