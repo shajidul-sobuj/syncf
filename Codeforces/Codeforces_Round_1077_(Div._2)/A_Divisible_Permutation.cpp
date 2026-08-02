3509efeba9e869328ef101bbbc8a47e67c6240fb
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-08-02 22:23:50 +0600

    [Sync] Divisible_Permutation on Codeforces 🚀

---
+++ Codeforces/Codeforces_Round_1077_(Div._2)/A_Divisible_Permutation.cpp
@@ -0,0 +1,45 @@
+/*
+ * Codeforces Auto Push - Synced Solution
+ * 
+ * Problem: 2188A - Divisible_Permutation
+ * Link: https://codeforces.com/contest/2188/problem/A
+ * Submission ID: 362010631
+ * Language: C++17 (GCC 7-32)
+ * Execution Time: 31 ms | Memory: N/A
+ * Verdict: OK
+ * Rating: 800 | Tags: constructive algorithms
+ * Submitted At: Sun, 08 Feb 2026 13:32:04 GMT
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vector <int> a(n);
+    for (int i=n-2,val=n; i>=0; i=i-2,val--)
+    {
+        a[i] = val;
+    }
+    for (int i=n-1,val=1; i>=0; i=i-2,val++)
+    {
+        a[i] = val;
+    }
+    for (int x : a)
+    {
+        cout << x << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        solve();
+    }
+    return 0;
+}
