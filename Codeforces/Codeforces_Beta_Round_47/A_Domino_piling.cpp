3f7423c548b4ce89b116918a533c187fc407d931
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-08-02 22:24:17 +0600

    [Sync] Domino_piling on Codeforces 🚀

---
+++ Codeforces/Codeforces_Beta_Round_47/A_Domino_piling.cpp
@@ -0,0 +1,23 @@
+/*
+ * Codeforces Auto Push - Synced Solution
+ * 
+ * Problem: 50A - Domino_piling
+ * Link: https://codeforces.com/contest/50/problem/A
+ * Submission ID: 362044865
+ * Language: C++17 (GCC 7-32)
+ * Execution Time: 92 ms | Memory: 100 KB
+ * Verdict: OK
+ * Rating: 800 | Tags: greedy, math
+ * Submitted At: Sun, 08 Feb 2026 18:16:59 GMT
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n,m;
+    cin >> n >> m;
+    int ans = (n*m)/2;
+    cout << ans << endl;
+    return 0;
+}
