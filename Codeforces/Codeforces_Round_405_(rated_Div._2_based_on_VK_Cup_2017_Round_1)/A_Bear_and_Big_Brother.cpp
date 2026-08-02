4ec5d39f2a8e1dc2d9698eb729048d995708c51c
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-08-02 22:24:57 +0600

    [Sync] Bear_and_Big_Brother on Codeforces 🚀

---
+++ Codeforces/Codeforces_Round_405_(rated_Div._2_based_on_VK_Cup_2017_Round_1)/A_Bear_and_Big_Brother.cpp
@@ -0,0 +1,34 @@
+/*
+ * Codeforces Auto Push - Synced Solution
+ * 
+ * Problem: 791A - Bear_and_Big_Brother
+ * Link: https://codeforces.com/contest/791/problem/A
+ * Submission ID: 362805816
+ * Language: C++17 (GCC 7-32)
+ * Execution Time: 31 ms | Memory: N/A
+ * Verdict: OK
+ * Rating: 800 | Tags: implementation
+ * Submitted At: Fri, 13 Feb 2026 17:32:26 GMT
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long int a,b;
+    cin >> a >> b;
+    int count = 0;
+    while(1)
+    {
+        if (a>b)
+        {
+            break;
+        }
+        count++;
+        a = a*3;
+        b = b*2;
+        
+    }
+    cout << count << endl;
+    return 0;
+}
