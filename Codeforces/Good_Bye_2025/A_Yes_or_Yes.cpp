78b55ce4b135a2a4a96488c16db90694f38acc16
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-08-02 22:19:24 +0600

    [Sync] Yes_or_Yes on Codeforces 🚀

---
+++ Codeforces/Good_Bye_2025/A_Yes_or_Yes.cpp
@@ -0,0 +1,38 @@
+/*
+ * Codeforces Auto Push - Synced Solution
+ * 
+ * Problem: 2178A - Yes_or_Yes
+ * Link: https://codeforces.com/contest/2178/problem/A
+ * Submission ID: 355346187
+ * Language: C++17 (GCC 7-32)
+ * Execution Time: 46 ms | Memory: N/A
+ * Verdict: OK
+ * Rating: 800 | Tags: greedy, strings
+ * Submitted At: Sat, 27 Dec 2025 15:05:41 GMT
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    int t;
+    cin >> t;
+    while (t--) {
+        string s;
+        cin >> s;
+        int freq = 0;
+        for (char c : s) {
+            if (c == &#39;Y&#39;) {
+                freq++;
+            }
+        }
+        
+        if (freq <= 1) {
+            cout << "YES" << "\n";
+        } else {
+            cout << "NO" << "\n";
+        }
+    }
+    return 0;
+}
