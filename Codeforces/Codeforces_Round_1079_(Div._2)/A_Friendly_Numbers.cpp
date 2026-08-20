4e2b8a5981148c2fb1364682535bfdb828d54c49
shajidul islam Sobuj <sajidislam1405@gmail.com>
2026-08-20 22:48:54 +0600

    [Sync] 2197A - Friendly Numbers 🚀

--- Codeforces/Codeforces_Round_1079_(Div._2)/A_Friendly_Numbers.cpp
+++ Codeforces/Codeforces_Round_1079_(Div._2)/A_Friendly_Numbers.cpp
@@ -1,15 +1,5 @@
-/*
- * Codeforces Auto Push - Synced Solution
- * 
- * Problem: 2197A - Friendly_Numbers
- * Link: https://codeforces.com/contest/2197/problem/A
- * Submission ID: 362431193
- * Language: C++17 (GCC 7-32)
- * Execution Time: 31 ms | Memory: 100 KB
- * Verdict: OK
- * Rating: 800 | Tags: binary search, brute force, expression parsing, math, schedules
- * Submitted At: Wed, 11 Feb 2026 14:54:22 GMT
- */
+// Code fetched by CPRepo (https://cprepo.vercel.app)
+// Platform: codeforces | Problem: 2197A - Friendly Numbers
 
 #include <bits/stdc++.h>
 using namespace std;
