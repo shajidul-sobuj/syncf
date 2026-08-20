676efb81f5ecda5fb7bc4741312c648f8265fd77
shajidul islam Sobuj <sajidislam1405@gmail.com>
2026-08-21 02:33:33 +0600

    [Sync] Palindrome Number 🚀

--- LeetCode/Practice/Palindrome_Number.cpp
+++ LeetCode/Practice/Palindrome_Number.cpp
@@ -5,8 +5,8 @@ public:
         if (x < 0) return false;
         while(x)
         {
-            int re = x%10;
-            v.push_back(re);
+            // int re = ;
+            v.push_back(x%10);
             x = x/10;
         }
         for (int i=0,j=v.size()-1;i<j;i++,j--)
