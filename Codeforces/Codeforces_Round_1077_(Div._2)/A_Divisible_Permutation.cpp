a62a58747c41f988ecf76fb79ca17231a3fce767
shajidul islam Sobuj <sajidislam1405@gmail.com>
2026-08-20 22:44:51 +0600

    [Sync] 2188A - Divisible Permutation 🚀

--- Codeforces/Codeforces_Round_1077_(Div._2)/A_Divisible_Permutation.cpp
+++ Codeforces/Codeforces_Round_1077_(Div._2)/A_Divisible_Permutation.cpp
@@ -1,45 +1,39 @@
-/*
- * Codeforces Auto Push - Synced Solution
- * 
- * Problem: 2188A - Divisible_Permutation
- * Link: https://codeforces.com/contest/2188/problem/A
- * Submission ID: 362010631
- * Language: C++17 (GCC 7-32)
- * Execution Time: 31 ms | Memory: N/A
- * Verdict: OK
- * Rating: 800 | Tags: constructive algorithms
- * Submitted At: Sun, 08 Feb 2026 13:32:04 GMT
- */
+// Code fetched by CPRepo (https://cprepo.vercel.app)
+// Platform: codeforces | Problem: 2188A - Divisible Permutation
 
 #include <bits/stdc++.h>
 using namespace std;
 
-void solve()
+int main()
 {
-    int n;
-    cin >> n;
-    vector <int> a(n);
-    for (int i=n-2,val=n; i>=0; i=i-2,val--)
-    {
-        a[i] = val;
-    }
-    for (int i=n-1,val=1; i>=0; i=i-2,val++)
-    {
-        a[i] = val;
-    }
-    for (int x : a)
-    {
-        cout << x << " ";
-    }
-    cout << endl;
-}
-
-int main() {
     int t;
     cin >> t;
-    while(t--)
+    while (t--)
     {
-        solve();
+        int n;
+        cin >> n;
+        int l = 1, r = n;
+        vector <int> v(n);
+        bool turn = false;
+
+        for (int i = n - 1; i >= 0; i--)
+        {
+            if (!turn)
+            {
+                v[i] = l++;
+            }
+            else
+            {
+                v[i] = r--;
+            }
+            turn = !turn;
+        }
+
+        for (int i = 0; i < n-1; i++)
+        {
+            cout << v[i] << " ";
+        }
+        cout << v[n-1] << endl;
     }
     return 0;
 }
