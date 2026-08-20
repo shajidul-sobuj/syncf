bd7df87bfba279d1f25a8b75e2383f50f0ade504
shajidul islam Sobuj <sajidislam1405@gmail.com>
2026-08-20 22:14:27 +0600

    [Sync] 110A - Nearly Lucky Number 🚀

--- Codeforces/Codeforces_Beta_Round_84_(Div._2_Only)/A_Nearly_Lucky_Number.cpp
+++ Codeforces/Codeforces_Beta_Round_84_(Div._2_Only)/A_Nearly_Lucky_Number.cpp
@@ -1,41 +1,22 @@
-/*
- * Codeforces Auto Push - Synced Solution
- * 
- * Problem: 110A - Nearly_Lucky_Number
- * Link: https://codeforces.com/contest/110/problem/A
- * Submission ID: 357100720
- * Language: C++17 (GCC 7-32)
- * Execution Time: 62 ms | Memory: 100 KB
- * Verdict: OK
- * Rating: 800 | Tags: implementation
- * Submitted At: Fri, 09 Jan 2026 15:07:00 GMT
- */
+// Code fetched by CPRepo (https://cprepo.vercel.app)
+// Platform: codeforces | Problem: 110A - Nearly Lucky Number
 
 #include <bits/stdc++.h>
 using namespace std;
-
 int main() {
     string s;
     cin >> s;
-    int cnt = 0;
-    for (char c : s) {
-        if (c == &#39;4&#39; || c == &#39;7&#39;) {
-            cnt++;
+    int sz = s.size();
+    int count=0;
+    for (int i=0;i<sz;i++) {
+        if (s[i]=='4' || s[i]=='7') {
+            count++;
         }
     }
-    if (cnt == 0) {
+    if ((count==4 || count==7) ) {
+        cout << "YES\n";
+    }else {
         cout << "NO\n";
-        return 0;
     }
-    while (cnt > 0) {
-        int d = cnt % 10;
-        if (d != 4 && d != 7) {
-            cout << "NO\n";
-            return 0;
-        }
-        cnt = cnt/10;
-    }
-    cout << "YES\n";
-
     return 0;
 }
