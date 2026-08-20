5c8bfbd39fff9aef6e927acac527ec7e4e8395c0
shajidul islam Sobuj <sajidislam1405@gmail.com>
2026-08-20 22:10:49 +0600

    [Sync] 281A - Word Capitalization 🚀

--- Codeforces/Codeforces_Round_172_(Div._2)/A_Word_Capitalization.cpp
+++ Codeforces/Codeforces_Round_172_(Div._2)/A_Word_Capitalization.cpp
@@ -1,15 +1,5 @@
-/*
- * Codeforces Auto Push - Synced Solution
- * 
- * Problem: 281A - Word_Capitalization
- * Link: https://codeforces.com/contest/281/problem/A
- * Submission ID: 354576833
- * Language: C++17 (GCC 7-32)
- * Execution Time: 92 ms | Memory: 100 KB
- * Verdict: OK
- * Rating: 800 | Tags: implementation, strings
- * Submitted At: Mon, 22 Dec 2025 14:52:12 GMT
- */
+// Code fetched by CPRepo (https://cprepo.vercel.app)
+// Platform: codeforces | Problem: 281A - Word Capitalization
 
 #include <bits/stdc++.h>
 using namespace std;
