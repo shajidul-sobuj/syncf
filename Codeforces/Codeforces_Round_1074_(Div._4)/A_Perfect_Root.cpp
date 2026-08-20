57c886c5827bed3d12a41c9b2282c7b1f562bc5c
shajidul islam Sobuj <sajidislam1405@gmail.com>
2026-08-20 22:30:17 +0600

    [Sync] 2185A - Perfect Root 🚀

--- Codeforces/Codeforces_Round_1074_(Div._4)/A_Perfect_Root.cpp
+++ Codeforces/Codeforces_Round_1074_(Div._4)/A_Perfect_Root.cpp
@@ -1,15 +1,5 @@
-/*
- * Codeforces Auto Push - Synced Solution
- * 
- * Problem: 2185A - Perfect_Root
- * Link: https://codeforces.com/contest/2185/problem/A
- * Submission ID: 358504640
- * Language: C++17 (GCC 7-32)
- * Execution Time: 31 ms | Memory: 100 KB
- * Verdict: OK
- * Rating: 800 | Tags: constructive algorithms, math
- * Submitted At: Sun, 18 Jan 2026 14:48:17 GMT
- */
+// Code fetched by CPRepo (https://cprepo.vercel.app)
+// Platform: codeforces | Problem: 2185A - Perfect Root
 
 #include <bits/stdc++.h>
 using namespace std;
