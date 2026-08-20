1e4c1359221b2d09b7755fe781e10639908562fa
shajidul islam Sobuj <sajidislam1405@gmail.com>
2026-08-20 22:37:49 +0600

    [Sync] 58A - Chat room 🚀

--- Codeforces/Codeforces_Beta_Round_54_(Div._2)/A_Chat_room.cpp
+++ Codeforces/Codeforces_Beta_Round_54_(Div._2)/A_Chat_room.cpp
@@ -1,52 +1,45 @@
-/*
- * Codeforces Auto Push - Synced Solution
- * 
- * Problem: 58A - Chat_room
- * Link: https://codeforces.com/contest/58/problem/A
- * Submission ID: 382564125
- * Language: C++23 (GCC 14-64, msys2)
- * Execution Time: 46 ms | Memory: N/A
- * Verdict: OK
- * Rating: 1000 | Tags: greedy, strings
- * Submitted At: Tue, 14 Jul 2026 14:09:22 GMT
- */
+// Code fetched by CPRepo (https://cprepo.vercel.app)
+// Platform: codeforces | Problem: 58A - Chat room
 
 #include <bits/stdc++.h>
 using namespace std;
 
-#define ll long long
-#define ull unsigned long long
-#define all(x) (x).begin(), (x).end()
-#define endl &#39;\n&#39;
-const int N = 1e5+10;
-
-void boom()
-{
-    string s; cin >> s;
-    stack<char> st;
-    string t = "olleh";
-    for (char c:t) st.push(c);
-    for (int i=0;i<s.size();i++){
-        if(!st.empty() && s[i] == st.top()) st.pop();
-    }
-    if(st.empty()) cout << "YES\n";
-    else cout << "NO\n";
-    
-
-}
-
-
 int main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    int t = 1;
-    // cin >> t;
-    while (t--)
+    string s;
+    cin >> s;
+    if (s.size()<5) 
     {
-        boom();
+        cout << "NO\n";
+        return 0;
     }
+    vector <char> vs;
+    for (char c : s)
+    {
+        vs.push_back(c);
+    }
+    string t = "hello";
+    
+    for (int i=0;i<5;i++)
+    {
+        bool has = false;
+        for (int j=0;j<vs.size();j++)
+        {
+            if (t[i]==vs[j])
+            {
+                vs.erase(vs.begin(),vs.begin()+j+1);
+                has = true;
+                break;
+            }
+            
+        }
+        if(!has)
+        {
+            cout << "NO\n";
+            return 0;
+        }
+    }
+    cout << "YES\n";
 
     return 0;
 }
