c5f930663c087d1c829ed50321a04a57ceec2b8d
shajidul islam Sobuj <sajidislam1405@gmail.com>
2026-08-20 22:18:31 +0600

    [Sync] 110A - Nearly Lucky Number 🚀

--- Codeforces/Codeforces_Beta_Round_84_(Div._2_Only)/A_Nearly_Lucky_Number.cpp
+++ Codeforces/Codeforces_Beta_Round_84_(Div._2_Only)/A_Nearly_Lucky_Number.cpp
@@ -3,20 +3,29 @@
 
 #include <bits/stdc++.h>
 using namespace std;
+
 int main() {
     string s;
     cin >> s;
-    int sz = s.size();
-    int count=0;
-    for (int i=0;i<sz;i++) {
-        if (s[i]=='4' || s[i]=='7') {
-            count++;
+    int cnt = 0;
+    for (char c : s) {
+        if (c == '4' || c == '7') {
+            cnt++;
         }
     }
-    if ((count==4 || count==7) ) {
-        cout << "YES\n";
-    }else {
+    if (cnt == 0) {
         cout << "NO\n";
+        return 0;
     }
+    while (cnt > 0) {
+        int d = cnt % 10;
+        if (d != 4 && d != 7) {
+            cout << "NO\n";
+            return 0;
+        }
+        cnt = cnt/10;
+    }
+    cout << "YES\n";
+
     return 0;
 }
