f047c8fd90733dfeb309bbb01af14f4050e021f5
shajidul islam Sobuj <sajidislam1405@gmail.com>
2026-08-20 21:58:19 +0600

    [Sync] 231A - Team 🚀

---
+++ Codeforces/2026-08/231A__Team.cpp
@@ -0,0 +1,25 @@
+// Code fetched by CPRepo (https://cprepo.vercel.app)
+// Platform: codeforces | Problem: 231A - Team
+
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    int a[n][3];
+    for (int i=0;i<n;i++) {
+        for (int j=0;j<3;j++) {
+            cin >> a[i][j];
+        }
+    }
+    int count=0;
+    for (int i=0;i<n;i++) {
+        int ck=0;
+        for (int j=0;j<3;j++) {
+            ck+=a[i][j];
+        }
+        if (ck>=2) count++;
+    }
+    cout << count << endl;
+    return 0;
+}
