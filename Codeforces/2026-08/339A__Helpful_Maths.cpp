f1fdd27c8cf9c97754f9de52fc76acd1b4f4ca4c
shajidul islam Sobuj <sajidislam1405@gmail.com>
2026-08-20 21:55:19 +0600

    [Sync] 339A - Helpful Maths 🚀

---
+++ Codeforces/2026-08/339A__Helpful_Maths.cpp
@@ -0,0 +1,19 @@
+// Code fetched by CPRepo (https://cprepo.vercel.app)
+// Platform: codeforces | Problem: 339A - Helpful Maths
+
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    string s;
+    cin >> s;
+    sort(s.begin(),s.end());
+    // cout << s[0];
+    for (int i=0;i<s.size()-1;i++) {
+       if (s[i]>='1' && s[i]<='9') {
+            cout << s[i];
+            cout << "+";
+       }
+    }
+    cout << s[s.size()-1] << endl;
+    return 0;
+}
