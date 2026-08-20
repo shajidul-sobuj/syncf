f982b0f764705aeebe9f77ea19b8a0e31b49b279
shajidul islam Sobuj <sajidislam1405@gmail.com>
2026-08-20 22:43:52 +0600

    [Sync] 236A - Boy or Girl 🚀

--- Codeforces/Codeforces_Round_146_(Div._2)/A_Boy_or_Girl.cpp
+++ Codeforces/Codeforces_Round_146_(Div._2)/A_Boy_or_Girl.cpp
@@ -1,15 +1,5 @@
-/*
- * Codeforces Auto Push - Synced Solution
- * 
- * Problem: 236A - Boy_or_Girl
- * Link: https://codeforces.com/contest/236/problem/A
- * Submission ID: 360099849
- * Language: C++17 (GCC 7-32)
- * Execution Time: 92 ms | Memory: 100 KB
- * Verdict: OK
- * Rating: 800 | Tags: brute force, implementation, strings
- * Submitted At: Tue, 27 Jan 2026 07:22:50 GMT
- */
+// Code fetched by CPRepo (https://cprepo.vercel.app)
+// Platform: codeforces | Problem: 236A - Boy or Girl
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -31,7 +21,7 @@ int main()
     int count = 0;
     for (int i=0;i<s.size();i++)
     {
-        if((s[i]!=0) && (s[i]>=&#39;a&#39; && s[i]<=&#39;z&#39;)) count++;
+        if((s[i]!=0) && (s[i]>='a' && s[i]<='z')) count++;
     }
     if (count%2!=0) cout << "IGNORE HIM!\n";
     else cout << "CHAT WITH HER!\n";
