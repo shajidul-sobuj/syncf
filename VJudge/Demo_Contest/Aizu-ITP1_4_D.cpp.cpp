212cef1722f9c8112ca0b327641ffd2f435b74a2
shajidul islam Sobuj <sajidislam1405@gmail.com>
2026-08-21 04:19:58 +0600

    [Sync] Aizu-ITP1_4_D 🚀

--- VJudge/Demo_Contest/Aizu-ITP1_4_D.cpp.cpp
+++ VJudge/Demo_Contest/Aizu-ITP1_4_D.cpp.cpp
@@ -1,5 +1,5 @@
 // Solution for VJudge Problem
-// Original URL: https://vjudge.net/solution/64745026
+// Original URL: https://vjudge.net/solution/64745068
 // Note: VJudge blocked access to the original source code.
 // Please check your VJudge account to view the code directly.
 
