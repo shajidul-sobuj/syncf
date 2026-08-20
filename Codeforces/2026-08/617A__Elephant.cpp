ac1cd7dd6d69ab42f216c8e7272fa36ebde89a82
shajidul islam Sobuj <sajidislam1405@gmail.com>
2026-08-20 22:02:20 +0600

    [Sync] 617A - Elephant 🚀

---
+++ Codeforces/2026-08/617A__Elephant.cpp
@@ -0,0 +1,17 @@
+// Code fetched by CPRepo (https://cprepo.vercel.app)
+// Platform: codeforces | Problem: 617A - Elephant
+
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int x;
+    cin >> x;
+    int count = 0;
+    for (int i=5;i>0;i--) {
+        int y = x/i;
+        count+=y;
+        x = x%i;
+    }
+    cout << count << endl;
+    return 0;
+}
