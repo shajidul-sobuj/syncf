92700fdc33ebe3f01f2b1dc954516c9fa6cf4a8e
shajidul islam Sobuj <sajidislam1405@gmail.com>
2026-08-20 22:56:57 +0600

    [Sync] 4A - Watermelon 🚀

--- Codeforces/Codeforces_Beta_Round_4_(Div._2_Only)/A_Watermelon.cpp
+++ Codeforces/Codeforces_Beta_Round_4_(Div._2_Only)/A_Watermelon.cpp
@@ -1,15 +1,5 @@
-/*
- * Codeforces Auto Push - Synced Solution
- * 
- * Problem: 4A - Watermelon
- * Link: https://codeforces.com/contest/4/problem/A
- * Submission ID: 382843143
- * Language: C++23 (GCC 14-64, msys2)
- * Execution Time: 62 ms | Memory: N/A
- * Verdict: OK
- * Rating: 800 | Tags: brute force, math
- * Submitted At: Wed, 15 Jul 2026 19:16:12 GMT
- */
+// Code fetched by CPRepo (https://cprepo.vercel.app)
+// Platform: codeforces | Problem: 4A - Watermelon
 
 #include <bits/stdc++.h>
 using namespace std;
