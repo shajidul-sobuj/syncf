9b96b7d7860862e48b20cf660c1aef543545e8bd
shajidul islam Sobuj <sajidislam1405@gmail.com>
2026-08-20 22:23:31 +0600

    [Sync] 2191C - Sorting Game 🚀

--- Codeforces/Codeforces_Round_1073_(Div._2)/C_Sorting_Game.cpp
+++ Codeforces/Codeforces_Round_1073_(Div._2)/C_Sorting_Game.cpp
@@ -1,15 +1,5 @@
-/*
- * Codeforces Auto Push - Synced Solution
- * 
- * Problem: 2191C - Sorting_Game
- * Link: https://codeforces.com/contest/2191/problem/C
- * Submission ID: 358318566
- * Language: C++17 (GCC 7-32)
- * Execution Time: 93 ms | Memory: N/A
- * Verdict: OK
- * Rating: 1200 | Tags: games
- * Submitted At: Sat, 17 Jan 2026 15:38:22 GMT
- */
+// Code fetched by CPRepo (https://cprepo.vercel.app)
+// Platform: codeforces | Problem: 2191C - Sorting Game
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -31,14 +21,14 @@ int main() {
         }
 
         int cnt0 = 0;
-        for (char ch : s) if (ch == &#39;0&#39;) cnt0++;
+        for (char ch : s) if (ch == '0') cnt0++;
 
         vector<int> pos;
         for (int i = 0; i < cnt0; i++) {
-            if (s[i] == &#39;1&#39;) pos.push_back(i + 1);
+            if (s[i] == '1') pos.push_back(i + 1);
         }
         for (int i = cnt0; i < n; i++) {
-            if (s[i] == &#39;0&#39;) pos.push_back(i + 1);
+            if (s[i] == '0') pos.push_back(i + 1);
         }
 
         cout << "Alice\n";
