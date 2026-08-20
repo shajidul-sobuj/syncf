c5750293496726774ca8e5af577e9339008c3674
shajidul islam Sobuj <sajidislam1405@gmail.com>
2026-08-20 22:59:59 +0600

    [Sync] 2188A - Divisible Permutation 🚀

---
+++ Codeforces/2026-08/2188A__Divisible_Permutation.cpp
@@ -0,0 +1,35 @@
+// Code fetched by CPRepo (https://cprepo.vercel.app)
+// Platform: codeforces | Problem: 2188A - Divisible Permutation
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vector <int> a(n);
+    for (int i=n-2,val=n; i>=0; i=i-2,val--)
+    {
+        a[i] = val;
+    }
+    for (int i=n-1,val=1; i>=0; i=i-2,val++)
+    {
+        a[i] = val;
+    }
+    for (int x : a)
+    {
+        cout << x << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        solve();
+    }
+    return 0;
+}
