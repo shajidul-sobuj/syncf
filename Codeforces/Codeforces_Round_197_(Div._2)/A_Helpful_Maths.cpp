c31ea441b9785aba0b36b3e56199d1e61ae3a495
shajidul islam Sobuj <sajidislam1405@gmail.com>
2026-08-20 22:09:47 +0600

    [Sync] 339A - Helpful Maths 🚀

--- Codeforces/Codeforces_Round_197_(Div._2)/A_Helpful_Maths.cpp
+++ Codeforces/Codeforces_Round_197_(Div._2)/A_Helpful_Maths.cpp
@@ -1,14 +1,19 @@
-/*
- * Codeforces Auto Push - Synced Solution
- * 
- * Problem: 339A - Helpful_Maths
- * Link: https://codeforces.com/contest/339/problem/A
- * Submission ID: 354434194
- * Language: C++17 (GCC 7-32)
- * Execution Time: 92 ms | Memory: N/A
- * Verdict: OK
- * Rating: 800 | Tags: greedy, implementation, sortings, strings
- * Submitted At: Sun, 21 Dec 2025 12:30:26 GMT
- */
+// Code fetched by CPRepo (https://cprepo.vercel.app)
+// Platform: codeforces | Problem: 339A - Helpful Maths
 
-#include <bits/stdc++.h>using namespace std;int main() {    string s;    cin >> s;    sort(s.begin(),s.end());    // cout << s[0];    for (int i=0;i<s.size()-1;i++) {       if (s[i]>='1' && s[i]<='9') {            cout << s[i];            cout << "+";       }    }    cout << s[s.size()-1] << endl;    return 0;}
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    string s;
+    cin >> s;
+    sort(s.begin(),s.end());
+    // cout << s[0];
+    for (int i=0;i<s.size()-1;i++) {
+       if (s[i]>='1' && s[i]<='9') {
+            cout << s[i];
+            cout << "+";
+       }
+    }
+    cout << s[s.size()-1] << endl;
+    return 0;
+}
