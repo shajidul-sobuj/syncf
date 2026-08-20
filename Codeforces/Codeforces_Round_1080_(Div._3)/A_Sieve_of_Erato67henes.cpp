98f25b9e86ca6dce049ae7e1a9e7a9320ad63b4f
shajidul islam Sobuj <sajidislam1405@gmail.com>
2026-08-20 22:54:57 +0600

    [Sync] 2195A - Sieve of Erato67henes 🚀

---
+++ Codeforces/Codeforces_Round_1080_(Div._3)/A_Sieve_of_Erato67henes.cpp
@@ -0,0 +1,37 @@
+// Code fetched by CPRepo (https://cprepo.vercel.app)
+// Platform: codeforces | Problem: 2195A - Sieve of Erato67henes
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i=0;i<n;i++) cin >> a[i];
+    bool yes = false;
+
+    for (int i=0;i<n;i++)
+    {
+         if(a[i]==67)
+        { 
+            yes = true;
+            break;
+        }
+    }
+    if (yes)
+        cout << "YES\n";
+    else
+        cout << "NO\n";
+}
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        solve();
+    }
+    return 0;
+}
