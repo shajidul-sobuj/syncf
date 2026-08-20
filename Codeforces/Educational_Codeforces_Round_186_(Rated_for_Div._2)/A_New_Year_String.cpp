07aaad055114805d8cb9f77ab74cc34212bc9c9e
shajidul islam Sobuj <sajidislam1405@gmail.com>
2026-08-20 22:16:28 +0600

    [Sync] 2182A - New Year String 🚀

--- Codeforces/Educational_Codeforces_Round_186_(Rated_for_Div._2)/A_New_Year_String.cpp
+++ Codeforces/Educational_Codeforces_Round_186_(Rated_for_Div._2)/A_New_Year_String.cpp
@@ -1,15 +1,5 @@
-/*
- * Codeforces Auto Push - Synced Solution
- * 
- * Problem: 2182A - New_Year_String
- * Link: https://codeforces.com/contest/2182/problem/A
- * Submission ID: 355726871
- * Language: C++17 (GCC 7-32)
- * Execution Time: 93 ms | Memory: N/A
- * Verdict: OK
- * Rating: 800 | Tags: constructive algorithms, greedy, implementation, strings
- * Submitted At: Mon, 29 Dec 2025 14:53:56 GMT
- */
+// Code fetched by CPRepo (https://cprepo.vercel.app)
+// Platform: codeforces | Problem: 2182A - New Year String
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -25,7 +15,7 @@ int main() {
             
             int opt1 = 0;
             for (int i = 0; i + 3 < n; i++) {
-                if (s[i] == &#39;2&#39; && s[i+1] == &#39;0&#39; && s[i+2] == &#39;2&#39; && s[i+3] == &#39;5&#39;) {
+                if (s[i] == '2' && s[i+1] == '0' && s[i+2] == '2' && s[i+3] == '5') {
                     opt1++;
                 }
             }
@@ -33,10 +23,10 @@ int main() {
             int opt2 = n; 
             for (int i = 0; i + 3 < n; i++) {
                 int curr = 0;
-                if (s[i] != &#39;2&#39;) curr++;
-                if (s[i+1] != &#39;0&#39;) curr++;
-                if (s[i+2] != &#39;2&#39;) curr++;
-                if (s[i+3] != &#39;6&#39;) curr++;
+                if (s[i] != '2') curr++;
+                if (s[i+1] != '0') curr++;
+                if (s[i+2] != '2') curr++;
+                if (s[i+3] != '6') curr++;
                 
                 if (curr < opt2) opt2 = curr;
             }
