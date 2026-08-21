ac10472b58e82b55e671f9bb01490e0e70fa3e37
shajidul islam Sobuj <sajidislam1405@gmail.com>
2026-08-21 11:20:03 +0600

    [Sync] Toph-neat-brackets 🚀

--- VJudge/TakeOff_Final_Preparation1/Toph-neat-brackets.cpp
+++ VJudge/TakeOff_Final_Preparation1/Toph-neat-brackets.cpp
@@ -1,5 +1,5 @@
 // Solution for VJudge Problem
-// Original URL: https://vjudge.net/solution/65732981
+// Original URL: https://vjudge.net/solution/65733001
 // Note: VJudge blocked access to the original source code.
 // Please check your VJudge account to view the code directly.
 
