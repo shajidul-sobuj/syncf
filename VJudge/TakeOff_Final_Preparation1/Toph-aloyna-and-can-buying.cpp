c6cbf23813cc4de1b912392c8b933a04511b8ca9
shajidul islam Sobuj <sajidislam1405@gmail.com>
2026-08-21 11:25:05 +0600

    [Sync] Toph-aloyna-and-can-buying 🚀

--- VJudge/TakeOff_Final_Preparation1/Toph-aloyna-and-can-buying.cpp
+++ VJudge/TakeOff_Final_Preparation1/Toph-aloyna-and-can-buying.cpp
@@ -1,5 +1,5 @@
 // Solution for VJudge Problem
-// Original URL: https://vjudge.net/solution/65733361
+// Original URL: https://vjudge.net/solution/65733380
 // Note: VJudge blocked access to the original source code.
 // Please check your VJudge account to view the code directly.
 
