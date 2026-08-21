8f959170ff5ea090b2878fbbe45675b388638eef
shajidul islam Sobuj <sajidislam1405@gmail.com>
2026-08-21 12:19:47 +0600

    [Sync] EOlymp-138 🚀

--- VJudge/Lab_Performance__2_CSE69I2/EOlymp-138.cpp
+++ VJudge/Lab_Performance__2_CSE69I2/EOlymp-138.cpp
@@ -1,5 +1,5 @@
 // Solution for VJudge Problem
-// Original URL: https://vjudge.net/solution/65879003
+// Original URL: https://vjudge.net/solution/65879097
 // Note: VJudge blocked access to the original source code.
 // Please check your VJudge account to view the code directly.
 
