c841b470e158b6c317be9bf8045460672661fc28
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-08-23 19:55:45 +0600

    [Codeforces] IQ test

---
+++ Codeforces/Codeforces_Beta_Round_25_(Div._2_Only)/25A_IQ_test.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int a[n+1];
+    for (int i=1;i<=n;i++)
+    {
+        cin >> a[i];
+    }
+    int oc = 0;
+    int ec = 0;
+    for (int i=1;i<=n;i++)
+    {
+        if (a[i]%2==0) ec++;
+        if (a[i]%2!=0) oc++;
+    }
+    int idx;
+    if (ec>oc) 
+    {
+        for (int i=1;i<=n;i++)
+        {
+            if (a[i]%2!=0) 
+            {
+                cout << i << endl;
+                return 0;
+            }
+        }
+    }else 
+    {
+        for (int i=1;i<=n;i++)
+        {
+            if (a[i]%2==0) 
+            {
+                cout << i << endl;
+                return 0;
+            }
+        }
+    }
+    return 0;
+}
