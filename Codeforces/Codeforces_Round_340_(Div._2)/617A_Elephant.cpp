367e34296bca10f7c6c896af9bdedb923ea1ebaf
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-08-23 20:01:45 +0600

    [Codeforces] Elephant

---
+++ Codeforces/Codeforces_Round_340_(Div._2)/617A_Elephant.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int x;
+    cin >> x;
+    int count = 0;
+    for (int i=5;i>0;i--) {
+        int y = x/i;
+        count+=y;
+        x = x%i;
+    }
+    cout << count << endl;
+    return 0;
+}
