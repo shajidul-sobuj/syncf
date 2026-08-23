c02b0cee1b5d5969d97f34fc635466c577aea331
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-08-23 19:52:53 +0600

    [Codeforces] Social Experiment

---
+++ Codeforces/Codeforces_Round_1072_(Div._3)/2184A_Social_Experiment.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int x;
+        cin >> x;
+        if (x==2) {
+            cout << 2 << endl;
+            continue;
+        }else if (x==3) {
+            cout << 3 << endl;
+            continue;
+        }else if (x%2==0) {
+            cout << 0 << endl;
+            continue;
+        }else {
+            cout << 1 << endl;
+        }
+    }
+    return 0;
+}
