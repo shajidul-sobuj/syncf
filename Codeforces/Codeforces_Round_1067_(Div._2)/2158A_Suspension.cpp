ec40601ee90e117ac98acbaeece5047b79912812
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-08-23 19:58:25 +0600

    [Codeforces] Suspension

---
+++ Codeforces/Codeforces_Round_1067_(Div._2)/2158A_Suspension.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int t;
+    cin >> t;
+    for (int i=1;i<=t;i++) {
+        int n,y,r;
+        cin >> n >> y >> r;
+        int count = 0;
+        count = count + y/2;
+        count = count + r/1;
+        if (count >=n) {
+            cout << n << endl;
+        }else if (count < n) {
+            cout << count << endl;
+        }
+    }
+
+    return 0;
+}
