144bad7df5d00c90f82bd43f93dfd0df8b93a755
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-08-23 20:33:15 +0600

    [AtCoder] A. Multiple of 2 and N

---
+++ AtCoder/AtCoder_Beginner_Contest_102/abc102_a_A._Multiple_of_2_and_N.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc&#43;&#43;.h>
+using namespace std;
+
+int main() {
+    long long int n;
+    cin >> n;
+    if (n%2==0)
+    {
+        cout << n << endl;
+    }else{
+        cout << (n*2) << endl;
+    }
+    return 0;
+}
