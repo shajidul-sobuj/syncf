5719ee6081379ced92339d9551967577996fefcf
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-08-23 20:27:00 +0600

    [Codeforces] Sleeping Through Classes

---
+++ Codeforces/Codeforces_2173/2173A_Sleeping_Through_Classes.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    int n,k;
+    cin >> n >> k;
+    string v;
+    cin >> v;
+    int test = 0;
+    int count = 0;
+    for (int i=0;i<n;i++)
+    {
+        if (v[i] == '0' && test == 0 )
+        {
+            count++;
+        }else if (v[i] == '1')
+        {
+            test = k;
+        }else if (v[i] == '0' && test != 0 )
+        {
+            test--;
+        }
+    }
+    cout << count << endl;
+}
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        solve();
+    }
+    return 0;
+}
