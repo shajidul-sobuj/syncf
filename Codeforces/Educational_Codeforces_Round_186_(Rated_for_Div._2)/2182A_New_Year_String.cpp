6eb7f8441d78147696b61a0d79c07e7b12aa0ad2
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-08-23 19:47:50 +0600

    [Codeforces] New Year String

---
+++ Codeforces/Educational_Codeforces_Round_186_(Rated_for_Div._2)/2182A_New_Year_String.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    if (cin >> t) {
+        while (t--) {
+            int n;
+            cin >> n;
+            string s;
+            cin >> s;
+            
+            int opt1 = 0;
+            for (int i = 0; i + 3 < n; i++) {
+                if (s[i] == '2' && s[i+1] == '0' && s[i+2] == '2' && s[i+3] == '5') {
+                    opt1++;
+                }
+            }
+            
+            int opt2 = n; 
+            for (int i = 0; i + 3 < n; i++) {
+                int curr = 0;
+                if (s[i] != '2') curr++;
+                if (s[i+1] != '0') curr++;
+                if (s[i+2] != '2') curr++;
+                if (s[i+3] != '6') curr++;
+                
+                if (curr < opt2) opt2 = curr;
+            }
+            
+            if (opt1 < opt2) cout << opt1 << "\n";
+            else cout << opt2 << "\n";
+        }
+    }
+    return 0;
+}
