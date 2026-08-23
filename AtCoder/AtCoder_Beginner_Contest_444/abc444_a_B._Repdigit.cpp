a2b312738fbb799922c7bb1bd69c964ed70ed847
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-08-23 20:22:32 +0600

    [AtCoder] B. Repdigit

---
+++ AtCoder/AtCoder_Beginner_Contest_444/abc444_a_B._Repdigit.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc&#43;&#43;.h>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+    if (s[0]==s[1] &&  s[0]==s[2])
+    {
+        cout << &#34;Yes\n&#34;;
+    }else{
+        cout << &#34;No\n&#34;;
+    }
+    
+    return 0;
+}
